5c0071859c47942180bd4e13dee72c837924cada
Yasmin <yasmin312002@hotmail.com>
2019-12-16 19:18:55 -0800

    Added customer components

---
+++ src/Components/Customer.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+const Customer = (props) => {
+
+  return (
+    <div className="customer">
+      <div className="content">
+        <div className="customer__content-info">{props.address}</div>
+        <div className="customer__content-info">{props.city}, {props.state}</div>
+        <div className="customer__content-info">{props.postal}</div>
+
+        <div className="customer__content-info">{props.phone}</div>
+        <div className="customer__content-info">${props.account_credit} account credit</div>
+      </div>
+    </div>
+  )
+}
+
+Customer.propTypes = {
+  id: PropTypes.number,
+  name: PropTypes.string,
+  address: PropTypes.string,
+  city: PropTypes.string,
+  state: PropTypes.string,
+  postal: PropTypes.string,
+  phone: PropTypes.string,
+  account_credit: PropTypes.number,
+  created_at: PropTypes.instanceOf(Date),
+};
+
+export default Customer;
---
+++ src/Components/CustomerList.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import Customer from './Customer.js';
+
+// import './Customer.css';
+
+
+export default class CustomerList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      customers: []
+    };
+  }
+  
+  componentDidMount() {
+    // API get request here
+    console.log("customer")
+    this.getCustomers()
+  }
+
+  getCustomers = () => {
+    axios.get('http://localhost:3000/customers')
+      .then((response) => {
+        console.log(response)
+        this.setState({
+          customers: response.data
+        });
+      })
+      .catch((error) => {
+        this.setState({ error: error.message });
+      });
+  }
+     
+  render() {
+    const customers = this.state.customers.map((customer) => {
+      return (
+        <Customer
+          key = { customer.id }
+          name = { customer.name }
+          id = { customer.id }
+          address = { customer.address }
+          city = { customer.city }
+          state = { customer.state }
+          postal_code = { customer.postal_code }
+          phone = { customer.phone }
+          account_credit = { customer.account_credit }
+          movies_checked_out_count = { customer.movies_checked_out_count }
+        />
+      );
+    });
+
+    return (
+      <div>
+        {customers}
+        {/* <NewCardForm addCard={this.addCard} /> */}
+      </div>
+    )
+  }
+}
+
+CustomerList.propTypes = {
+
+};
+
+
