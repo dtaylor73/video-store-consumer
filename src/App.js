357601c566c3008c751e74d0600bb99ee4121720
Yasmin <yasmin312002@hotmail.com>
2019-12-17 14:56:43 -0800

    change App state - current customer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,85 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+// import axios from 'axios';
 import './App.css';
+import MovieLibrary from './Components/MovieLibrary.js';
+import MovieSearch from './Components/MovieSearch.js';
+import CustomerList from './Components/CustomerList.js';
 
+import Customer from './Components/Customer.js';
+import React, {Component} from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentCustomer: ''
+    };
+  }
+
+  customerNameCallback = (customer) => {
+    // console.log(customer)
+    // console.log(id)
+    this.setState({
+      currentCustomer: customer
+    });
+    console.log(this.state.currentCustomer)
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              {/* <li>
+                <Link to="/Home">Home</Link>
+              </li> */}
+              <li>
+                <Link to="/MovieSearch">Movie Search</Link>
+              </li>
+              <li>
+                <Link to="/MovieLibrary">Movie Library</Link>
+              </li>
+              <li>
+                <Link to="/CustomerList">Customer List</Link>
+              </li>
+            </ul>
+          </nav>
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          
+          <Switch>
+            {/* <Route path="/Home">
+              <Home />
+            </Route> */}
+            {/* <Route path="/MovieSearch">
+              <MovieSearch/>
+            </Route> */}
+            <Route path="/MovieLibrary">
+              <MovieLibrary />
+            </Route>
+            {/* <Route path="/MovieSearch">
+              <MovieSearch /> */}
+            {/* </Route> */}
+            <Route path="/Customer">
+              <Customer />
+            </Route>
+            <Route path="/CustomerList">
+              <CustomerList customerNameCallback={ this.customerNameCallback }
+             />
+              {/* {`this is your current customer! ${this.state.currentCustomer.name}`} */}
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
+
 
 export default App;
